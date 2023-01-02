a13d588e2475ebe7f212422b5ba4f5113422e7e3
gjimm07 <finn.h@hotmail.de>
2023-01-02 19:16:43 +0100

    gui changed

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 from LED_array import LEDArray
+
 #
 # LED Steuerung Eike
 #
@@ -7,18 +8,7 @@ from LED_array import LEDArray
 mat = LEDArray()
 mat.read_from_file()
 ARRAY_SIZE = 3, 3
-COLORS = ["Red", "Blue", "Green"]
-MAIN_MENU_OBJ = {"Static": {"All": {"Color": dict.fromkeys(COLORS, None)},
-                            "Row": {"Row": dict.fromkeys([str(i) for i in range(ARRAY_SIZE[0])], None),
-                                    "Color": dict.fromkeys(COLORS, None)},
-                            "Column": {"Column": dict.fromkeys([str(i) for i in range(ARRAY_SIZE[1])], None),
-                                       "Color": dict.fromkeys(COLORS, None)},
-                            "Single": {"Row": dict.fromkeys([str(i) for i in range(ARRAY_SIZE[0])], None),
-                                       "Column": dict.fromkeys([str(i) for i in range(ARRAY_SIZE[1])], None),
-                                       "Color": dict.fromkeys(COLORS, None)}},
-                 "Dynamic": {"Rain": None,
-                             "Sideways": None},
-                 "Sleep": None}
+COLORS = ("RED", "BLUE", "GREEN")
 
 
 class MenuObject:
@@ -28,7 +18,9 @@ class MenuObject:
         self.last = last
 
     def return_slaves_name(self):
-        return [i.name for i in self.slaves]
+        if not self.last:
+            return [i.name for i in self.slaves]
+        return self.slaves
 
     def return_slave(self, name):
         for i in self.slaves:
@@ -36,9 +28,9 @@ class MenuObject:
                 return i
 
 
-row = MenuObject(name="ROW", slaves=("a", "b"), last=True)
-column = MenuObject(name="COLUMN", slaves=("a", "b"), last=True)
-color = MenuObject(name="COLOR", slaves=("a", "b"), last=True)
+row = MenuObject(name="ROW", slaves=("A", "B"), last=True)
+column = MenuObject(name="COLUMN", slaves=("A", "B"), last=True)
+color = MenuObject(name="COLOR", slaves=COLORS, last=True)
 
 select_all = MenuObject(name="SELECT ALL", slaves=color)
 select_single = MenuObject(name="SELECT SINGLE", slaves=(row, column, color))
@@ -54,26 +46,60 @@ main_menu = MenuObject(name="MAIN MENU", slaves=(dynamic, static))
 
 
 def menu(master_order):
-    master = master_order[-1]
-    if not master.last:
-        selection = button_select(master.return_slaves_name())
-        master_order.append(master.return_slave(selection))
+    selection = ""
+    while True:
+        master = master_order[-1]
+        selected_items = yield master.last, selection, master.slaves
+        if not master.last:
+            selection = button_select(master.return_slaves_name(), [x.name for x in master_order], selected_items)
+            master_order.append(master.return_slave(selection))
+        else:
+            selection = button_select(master.return_slaves_name(), [x.name for x in master_order])
+            del master_order[-1]
         menu(master_order)
-    else:
-        print("reached")
+
+
+def main():
+    while True:
+        first_over_last, input_, key = False, False, ""
+        dict_ = {}
+        a = menu([main_menu])
+        next(a)
+        while True:
+            last, selection, slaves = a.send(dict_)
+            if not first_over_last:
+                if slaves[0].last:
+                    dict_ = dict.fromkeys([s.name for s in slaves], [False, ""])
+                    first_over_last = True
+            else:
+                if last:
+                    input_, key = True, selection
+                elif input_:
+                    dict_[key] = [True, selection]
+                    input_ = False
+            if False not in [s[0] for s in list(dict_.values())] and dict_ != {}:
+                print(dict_)
+                break
 
 
 def turn_on_row():
     print("turn on row")
 
 
-def button_select(items):
+def button_select(items, header, s=None):
+    print("\n")
+    print(header)
+    if s != {} and s is not None:
+        z = [item + "-d" if bool_ else item for item, bool_ in zip(items, [x[0] for x in list(s.values())])]
+    else:
+        z = items
     while True:
-        selection = input("Please select from the following {}: ".format(items))
+        selection = input("Please select from the following {}: ".format(z))
         if selection.upper() in str(items):
             return selection.upper()
+        elif selection.upper() == "BACK":
+            return "BACK"
 
 
 if __name__ == "__main__":
-    menu([main_menu])
-
+    main()
